d736926831d1828187e273915cb0c269cee3eb99
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-09-11 18:23:48 -0300

    path routes fix

--- src/infra/routes/index.ts
+++ src/infra/routes/index.ts
@@ -1,19 +1,20 @@
 import { readdirSync } from "fs";
-import { Router } from "express"
+import path from "node:path";
+import { Response, Router } from "express"
 const router: Router = Router()
 // import { router as testRouter } from "./test.routes"
 
-router.get("/", (__, res) => {
+router.get("/", (__, res: Response) => {
     res.send("Welcome to Magenta productivity app API");
 });
-router.get('/sensei', (__, res) => {
+router.get('/sensei', (__, res: Response) => {
     res.send('El facusama te bendice.')
 })
 
 // router.use('/test', testRouter)
 
 //? Toma todos los archivos de la carpeta routes, toma su nombre y lo utliza para crear un router
-const PATH_ROUTES = __dirname;
+const PATH_ROUTES = path.resolve(process.cwd() + "/src/infra/routes");
 
 function removeExtension(fileName: string): string {
     const cleanFileName = <string>fileName.split(".").shift();
