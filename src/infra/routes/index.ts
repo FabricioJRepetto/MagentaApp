1f2dab9e286eafb853206297c9bcd0381f1114d4
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-09-11 18:28:11 -0300

    classic routes declaration

--- src/infra/routes/index.ts
+++ src/infra/routes/index.ts
@@ -2,7 +2,7 @@ import { readdirSync } from "fs";
 import path from "node:path";
 import { Response, Router } from "express"
 const router: Router = Router()
-// import { router as testRouter } from "./test.routes"
+import { router as testRouter } from "./test.routes"
 
 router.get("/", (__, res: Response) => {
     res.send("Welcome to Magenta productivity app API");
@@ -11,30 +11,30 @@ router.get('/sensei', (__, res: Response) => {
     res.send('El facusama te bendice.')
 })
 
-// router.use('/test', testRouter)
+router.use('/test', testRouter)
 
-//? Toma todos los archivos de la carpeta routes, toma su nombre y lo utliza para crear un router
-const PATH_ROUTES = path.resolve(process.cwd() + "/src/infra/routes");
+// //? Toma todos los archivos de la carpeta routes, toma su nombre y lo utliza para crear un router
+// const PATH_ROUTES = path.resolve(process.cwd() + "/src/infra/routes");
 
-function removeExtension(fileName: string): string {
-    const cleanFileName = <string>fileName.split(".").shift();
-    return cleanFileName;
-}
+// function removeExtension(fileName: string): string {
+//     const cleanFileName = <string>fileName.split(".").shift();
+//     return cleanFileName;
+// }
 
-/**
- *
- * @param file tracks.ts
- */
-function loadRouter(file: string): void {
-    const name = removeExtension(file);
-    if (name !== "index") {
-        import(`./${file}`).then((routerModule) => {
-            console.log(`\x1b[32m > route ${name} loaded \x1b[0m`);
-            router.use(`/${name}`, routerModule.router);
-        });
-    }
-}
+// /**
+//  *
+//  * @param file tracks.ts
+//  */
+// function loadRouter(file: string): void {
+//     const name = removeExtension(file);
+//     if (name !== "index") {
+//         import(`./${file}`).then((routerModule) => {
+//             console.log(`\x1b[32m > route ${name} loaded \x1b[0m`);
+//             router.use(`/${name}`, routerModule.router);
+//         });
+//     }
+// }
 
-readdirSync(PATH_ROUTES).filter((file) => loadRouter(file));
+// readdirSync(PATH_ROUTES).filter((file) => loadRouter(file));
 
 export default router
