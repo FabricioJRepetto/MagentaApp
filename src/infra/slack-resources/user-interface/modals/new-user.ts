adf571f5a86a6120cf0ed38bec5792c0f47d5b48
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-09-20 14:08:00 -0300

    new user modal correction

--- src/infra/slack-resources/user-interface/modals/new-user.ts
+++ src/infra/slack-resources/user-interface/modals/new-user.ts
@@ -4,26 +4,24 @@ export default (): string => {
     const modal = Modal({ title: "Crear usuario", submit: "Enviar", close: "cancelar", callbackId: "user_signin" })
         .blocks(
             Blocks.Section({ text: "Completa los datos de tu cuenta para poder comenzar a registrar actividades" }),
-            Blocks.Input({ label: "Nombre y Apellido", blockId: "name" }).element(
-                Elements.TextInput({
-                    actionId: "name_input"
-                })
-            ),
             Blocks.Divider(),
 
-            Blocks.Input({ label: "Email", blockId: "mail" }).element(
+            Blocks.Input({ label: "Nombre y Apellido", blockId: "name" }).element(
                 Elements.TextInput({
-                    actionId: "mail_input"
+                    actionId: "name_input",
+                    placeholder: ""
                 })
             ),
             Blocks.Input({ label: "Email", blockId: "email" }).element(
                 Elements.TextInput({
-                    actionId: "email_input"
+                    actionId: "email_input",
+                    placeholder: ""
                 })
             ),
             Blocks.Input({ label: "Teléfono", blockId: "phone" }).element(
                 Elements.TextInput({
-                    actionId: "phone_input"
+                    actionId: "phone_input",
+                    placeholder: ""
                 })
             )
         ).buildToJSON();
